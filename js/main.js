551c3aa83f5730195593adce5e5cb099db5bee39
melaniegrcia <mat.matsu89@gmail.com>
2019-05-22 21:49:48 -0300

    Más cambios en index y ayuda

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
-$(document).scroll(function(event) {
-	if($(document).scrollTop() > 300) {
-		$("#subir").addClass("mostrar");
-		$("#subir").removeClass("ocultar");
-	} else {
-		$("#subir").addClass("ocultar");
-		$("#subir").removeClass("mostrar");
-	}
-});
+$(document).ready(function(){
+	/*	FUNCION PARA OCULTAR O MOSTRAR EL BOTON PARA SUBIR EN LA PAGINA DE AYUDA 	*/
+	$(document).scroll(function(event) {
+		if($(document).scrollTop() > 300) {
+			$("#subir").addClass("mostrar");
+			$("#subir").removeClass("ocultar");
+		} else {
+			$("#subir").addClass("ocultar");
+			$("#subir").removeClass("mostrar");
+		}
+	});
 
-$("#subir").click(function(){
-	$(window).scrollTop(0,0);
+	/*	FUNCION PARA IR AL TOPE DE LA PÁGINA AYUDA 	*/
+	$("#subir").click(function(){
+		var a = setInterval(function () {
+			$(window).scrollTop($(document).scrollTop()-100)
+			if ($(document).scrollTop() < 0) {
+				clearInterval(a);
+			}
+		},30);	
+	});
+
+	/*	CREAR FUNCION PARA QUE EL ANCHOR EN AYUDA FUNCIONE BIEN 	*/
+	$(".indice>ul li a").on("click", function() {
+		//$(document).scrollTop($(document).scrollTop() + 1000);
+	});
+
+
+	/*	MOSTRAR Y OCUTAR COMENTARIOS EN INDEX.HTML 	*/
+	$(".verComentarios").click(function() {
+		if($(this).attr("aria-expanded")==="true") {
+			$(this).text("Ver comentarios");
+		} else {
+			$(this).text("Ocultar comentarios");
+		}
+	});
+
+	$("#flag").click(function() {
+		$(this).toggleClass("redFlag");
+	});
+
 });
