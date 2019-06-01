5e3a5e3b0cb3291c8d20f2be65efa9ebc0f7dc08
melaniegrcia <mat.matsu89@gmail.com>
2019-05-31 21:55:33 -0300

    Cambios en index y login. Agregado de comentarios

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+	/*	AVISO DE QUE ESTAS SALIENDO DE LA PÁGINA		*/
+	$("#logout").click(_=>{
+		//Cuando se oscurezca la pantalla tira un confirmBox
+		$.when($("body").append("<div class='fondoOscuro'></div>")).then(_=>{
+			if (confirm('¿Estás seguro de volver a una vida "normal"?')) {
+				//llevar a login
+				alert("Ya volveras a buscar cobijo aquí bodoque!");
+				window.location.replace("./paginas/login.html");
+			} else {
+				//Hacer nada
+				alert("Era lo que me imaginaba...");
+				$(".fondoOscuro").remove();
+			}
+		});
+	});
+	/*************************************************************/
+
+	/*************************************************************/
 	/*	FUNCION PARA OCULTAR O MOSTRAR EL BOTON PARA SUBIR EN LA PAGINA DE AYUDA 	*/
 	$(document).scroll(function(event) {
 		if($(document).scrollTop() > 300) {
@@ -9,7 +27,9 @@ $(document).ready(function(){
 			$("#subir").removeClass("mostrar");
 		}
 	});
+	/*************************************************************/
 
+	/*************************************************************/
 	/*	FUNCION PARA IR AL TOPE DE LA PÁGINA AYUDA 	*/
 	$("#subir").click(function(){
 		var a = setInterval(function () {
@@ -19,8 +39,26 @@ $(document).ready(function(){
 			}
 		},30);	
 	});
+	/*************************************************************/
 
-	/*	MOSTRAR Y OCUTAR COMENTARIOS EN INDEX.HTML 	*/
+	/*************************************************************/
+	/*	ACTIVA Y DESACTIVA EL BOTON PARA PUBLICAR MENSAJES EN EL MURO	*/
+	$("textarea").focus(_=>{
+		$("textarea").bind("keyup", function(e){
+			var comment = $.trim($(this).val());
+			if(comment != "") {
+				//SI ES DISTINTO A "" ACTIVA EL BOTON
+				$(this).siblings("div").children("input").removeAttr("disabled");
+			} else {
+				//SI ES IGUAL A "" DESACTIVA EL BOTON
+				$(this).siblings("div").children("input").attr("disabled", true);
+			}
+		});
+	});
+	/*************************************************************/
+
+	/*************************************************************/
+	/*	MOSTRAR Y OCULTAR COMENTARIOS EN INDEX.HTML 	*/
 	$(".verComentarios").click(function() {
 		if($(this).attr("aria-expanded")==="true") {
 			$(this).text("Ver comentarios");
@@ -28,9 +66,26 @@ $(document).ready(function(){
 			$(this).text("Ocultar comentarios");
 		}
 	});
+	/*************************************************************/
 
-	$("#flag").click(function() {
+	/*************************************************************/
+	/*	CAMBIA COLOR DE LA BANDERA 	*/
+	$(".flag").click(function() {
+		if($("i", this).hasClass("fas")) {
+			$(".fas",this).remove();
+			$(this).append("<i class='far fa-flag'></i>");
+		} else {
+			$(".far",this).remove();
+			$(this).append("<i class='fas fa-flag'></i>");
+		}
 		$(this).toggleClass("redFlag");
 	});
+	/*************************************************************/
 
+	/*************************************************************/
+	/*	CAMBIA COLOR DE LA BANDERA 	*/
+	$("#leyes").click(function() {
+		$(".cb").prop("checked", true);
+	});
+	/*************************************************************/
 });
